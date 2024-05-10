a0c8b8904e332955b12f3afffdbc4942a5b796c8
GabrielFuschter <108005423+GabrielFuschter@users.noreply.github.com>
2024-05-10 10:47:15 -0300

    10\5

--- main.js
+++ main.js
@@ -14,11 +14,14 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-const contadores = document.querySelectorAll(".contador")
-
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-18T00:00:00");
+const tempoObjetivo2 = new Date("2024-07-15T00:00:00");
+const tempoObjetivo3 = new Date("2024-04-01T00:00:00");
+const tempoObjetivo4 = new Date("2024-11-20T00:00:00");
 
-contadores[0].textContent = calculaTempo (tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo (tempoObjetivo){
     let tempoAtual = new Date();
@@ -32,6 +35,24 @@ function calculaTempo (tempoObjetivo){
     minutos %= 60;
     horas %= 24;
 
-
-
-}
+    if (tempoFinal>0){
+        return [dias, horas, minutos, segundos];
+        } else {
+        return[0,0,0,0];
+        }
+        }
+        
+        function atualizaCronometro(){
+            for(let i=0; i<contadores.length; i++) {
+                document.getElementById("dias"+1).textContent=calculaTempo(tempos[i])[0];
+                document.getElementById("horas"+1).textContent=calculaTempo(tempos[i])[1];
+                document.getElementById("min"+1).textContent=calculaTempo(tempos[i])[2];
+                document.getElementById("seg"+1).textContent=calculaTempo(tempos[i])[3];
+        }
+        }
+        function comecaCronometro(){
+        atualizaCronometro();
+        setInterval(atualizaCronometro, 1000);
+        }
+        
+        comecaCronometro();
